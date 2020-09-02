4259e67093b2de36f606353712576ccb0bfe968c
Rafadana Mountheira <rafadanam@gmail.com>
2020-09-02 09:40:14 +0700

    refactor the task lists to another file

--- lib/screens/task_page.dart
+++ lib/screens/task_page.dart
@@ -24,53 +24,7 @@ class TaskPage extends StatelessWidget {
               height: _height * 0.025,
             ),
             Expanded(
-              child: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    height: 10.0,
-                  );
-                },
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF064B41),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(15.0),
-                      ),
-                    ),
-                    child: Center(
-                      child: ListTile(
-                        title: Text(
-                          'Task Name',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 5.0),
-                          child: Text(
-                            '6 September 1969',
-                            style: TextStyle(
-                                color: Color(0xFF73A99C), fontSize: 13.0),
-                          ),
-                        ),
-                        isThreeLine: true,
-                        trailing: Theme(
-                          data: ThemeData(
-                              unselectedWidgetColor: Color(0xFF73A99C)),
-                          child: Checkbox(
-                            value: false,
-                            onChanged: (bool value) {},
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                itemCount: 3,
-              ),
+              child:
             ),
           ],
         ),
