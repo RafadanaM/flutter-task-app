dbbb049c4499d032222b1a7ab7d9189f08d66df5
Rafadana Mountheira <rafadanam@gmail.com>
2020-09-02 13:07:51 +0700

    add Title to detail screen

--- lib/screens/task_detail_screen.dart
+++ lib/screens/task_detail_screen.dart
@@ -5,9 +5,42 @@ class TaskDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('DetailScreen'),
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color(0xFF064B41),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: _width,
+              height: _height * 0.2,
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(
+                    Icons.arrow_back,
+                    size: 40.0,
+                  ),
+                  Text(
+                    'Title',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
