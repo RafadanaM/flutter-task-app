891d31e410a684012e439870ffd6f3d184fd2c10
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-01-13 20:12:52 +0700

    add reminder with validation

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tasks_app/widgets/clickable_icon.dart';
 import 'package:flutter_material_pickers/flutter_material_pickers.dart';
+import 'package:intl/intl.dart';
 
 class AddTaskScreen extends StatefulWidget {
   static const routeName = '/add';
@@ -13,18 +14,32 @@ class AddTaskScreen extends StatefulWidget {
 class _AddTaskScreenState extends State<AddTaskScreen> {
   String stateText;
   DateTime _pickedDate;
+  DateTime _inputDateTime;
   TimeOfDay _pickedTime;
   String _title;
   String _description;
   int _reminder;
   double _height;
   double _width;
+  List<String> _minutes;
+  final DateFormat formatter = DateFormat('MMM dd, HH:mm');
 
   @override
   void initState() {
     super.initState();
+    _minutes = <String>[
+      '5 Minutes',
+      '10 Minutes',
+      '15 Minutes',
+      '20 Minutes',
+      '25 Minutes',
+      '30 Minutes',
+    ];
     _pickedDate = DateTime.now();
     _pickedTime = TimeOfDay.now();
+    _reminder = 0;
+    _inputDateTime = DateTime(_pickedDate.year, _pickedDate.month,
+        _pickedDate.day, _pickedTime.hour, _pickedTime.minute);
   }
 
   @override
@@ -69,11 +84,10 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                         TextField(
                           maxLines: 1,
                           style: TextStyle(
-                              color: Colors.grey,
+                              color: Colors.black,
                               fontWeight: FontWeight.bold,
                               fontSize: 34.0),
                           cursorColor: Colors.grey,
-                          autofocus: true,
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: 'Title',
@@ -102,7 +116,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                             color: Color(0xFF73A99C),
                             fontWeight: FontWeight.bold,
                             fontSize: 18.0),
-                        cursorColor: Colors.grey,
+                        cursorColor: Colors.white,
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           hintText: 'Description',
@@ -122,11 +136,11 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                         direction: Axis.horizontal,
                         iconData: Icons.calendar_today,
                         iconSize: 40,
-                        title:
-                            "${_pickedDate.day}/${_pickedDate.month}/${_pickedDate.year} ${_pickedTime.hour}:${_pickedTime.minute}",
+                        title: formatter.format(_inputDateTime),
                         titleSize: 18.0,
                         itemSpacing: 20.0,
                         onTap: () {
+                          FocusScope.of(context).unfocus();
                           _pickDateTime();
                         },
                       ),
@@ -137,10 +151,11 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                         direction: Axis.horizontal,
                         iconData: Icons.notifications,
                         iconSize: 40,
-                        title: 'Reminder',
+                        title: _reminder == 0 ? 'Reminder' : _calcReminder(),
                         titleSize: 18.0,
                         itemSpacing: 20.0,
                         onTap: () {
+                          FocusScope.of(context).unfocus();
                           _dropDown();
                         },
                       ),
@@ -150,21 +165,28 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
               ],
             ),
           ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: RaisedButton(
-              color: Color(0xFFFF844C),
-              textColor: Colors.white,
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: ButtonTheme(
+                minWidth: 70,
+                height: 41,
+                child: RaisedButton(
+                  color: Color(0xFFFF844C),
+                  textColor: Colors.white,
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onPressed: () {
+                    print("===================");
+                    print(_title);
+                    print(_description);
+                  },
+                  child: Text('Save'),
+                ),
               ),
-              onPressed: () {
-                print("===================");
-                print(_title);
-                print(_description);
-              },
-              child: Text('Save'),
             ),
           )
         ],
@@ -189,9 +211,9 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
       );
       DateTime currentDateTime = DateTime.now();
       if (time != null) {
-        DateTime inputDateTime = DateTime(_pickedDate.year, _pickedDate.month,
+        _inputDateTime = DateTime(_pickedDate.year, _pickedDate.month,
             _pickedDate.day, time.hour, time.minute);
-        if (inputDateTime.isAfter(currentDateTime))
+        if (_inputDateTime.isAfter(currentDateTime))
           setState(() {
             _pickedTime = time;
           });
@@ -215,14 +237,26 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   // }
 
   _dropDown() {
-    return showMaterialNumberPicker(
-      context: context,
-      title: "Pick a number",
-      minNumber: 5,
-      maxNumber: 30,
-      step: 5,
-      selectedNumber: _reminder,
-      onChanged: (value) => setState(() => _reminder = value),
-    );
+    return showMaterialScrollPicker(
+        context: context,
+        title: "Reminder",
+        items: _minutes,
+        maxLongSide: 400,
+        maxShortSide: 300,
+        // selectedItem: selectedUsState,
+        onChanged: (value) {
+          List<String> split = value.split(" ");
+          int time = int.parse(split[0]);
+          if (DateTime.now()
+              .isBefore(_inputDateTime.subtract(Duration(minutes: time))))
+            setState(() {
+              _reminder = time;
+            });
+        });
+  }
+
+  _calcReminder() {
+    DateTime reminder = _inputDateTime.subtract(Duration(minutes: _reminder));
+    return formatter.format(reminder);
   }
 }
